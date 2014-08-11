023d07669c2f2fc86759d9223a8c789a37ae0b18
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-11 16:56:48 -0400

    World can be circumnavigated from any direction

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,58 +1,60 @@
 class Character < ActiveRecord::Base
   belongs_to :user
 
-  def can_move_east?
-    WORLD.terrain[x_coordinate + 1][y_coordinate]
-  end
-
-  def can_move_west?
-    WORLD.terrain[x_coordinate - 1][y_coordinate]
-  end
-
-  def can_move_north?
-    WORLD.terrain[x_coordinate][y_coordinate + 1]
-  end
-
-  def can_move_south?
-    WORLD.terrain[x_coordinate][y_coordinate - 1]
-  end
+  Y_BOUNDARY = World.first.terrain.length - 1
+  X_BOUNDARY = World.first.terrain[0].length - 1
 
   def move(direction)
     case direction
-
     when "east"
       move_east
-
     when "west"
       move_west
-
     when "north"
       move_north
-
     when "south"
       move_south
     end
+    save
+  end
+
+  def at_x_boundary?
+    x_coordinate != 0 && x_coordinate % X_BOUNDARY == 0
+  end
+
+  def at_y_boundary?
+    y_coordinate != 0 && y_coordinate % Y_BOUNDARY == 0
   end
 
   def move_east
-    self.x_coordinate += 1
-    save
+    if at_x_boundary?
+      self.x_coordinate = 0
+    else
+      self.x_coordinate += 1
+    end
   end
 
   def move_west
-    self.x_coordinate -= 1
-    save
+    if at_x_boundary?
+      self.x_coordinate = 0
+    else
+      self.x_coordinate -= 1
+    end
   end
 
   def move_north
-    self.y_coordinate -= 1
-    save
+    if at_y_boundary?
+      self.y_coordinate = 0
+    else
+      self.y_coordinate -= 1
+    end
   end
 
   def move_south
-    self.y_coordinate += 1
-    save
+    if at_y_boundary?
+      self.y_coordinate = 0
+    else
+      self.y_coordinate += 1
+    end
   end
 end
-
-
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,14 +1,21 @@
 class Map
-
   def initialize(character)
     @character = character
   end
 
   def display
-    map_of_terrain = Marshal.load(Marshal.dump(WORLD.terrain))
-    mark_character_coordinates(map_of_terrain)
+    terrain = duplicate_world
+    mark_character_coordinates(terrain)
+    map_terrain(terrain)
+  end
+
+  def duplicate_world
+    Marshal.load(Marshal.dump(WORLD.terrain))
+  end
+
+  def map_terrain(terrain)
     html_map = ""
-    map_of_terrain.each do |y_axis_rooms|
+    terrain.each do |y_axis_rooms|
       y_axis_rooms.each do |x_axis_rooms|
         x_axis_rooms.each do |room|
           html_map += room
@@ -19,7 +26,7 @@ class Map
     html_map
   end
 
-  def mark_character_coordinates(map_of_terrain)
-    map_of_terrain[@character.y_coordinate][@character.x_coordinate][0][0] = "*"
+  def mark_character_coordinates(terrain)
+    terrain[@character.y_coordinate][@character.x_coordinate][0][0] = "*"
   end
 end
