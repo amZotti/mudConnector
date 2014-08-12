0dee00c28737a04233e5ad3a46749d795699b34f
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-12 11:07:03 -0400

    WIP: Add characters index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811152651) do
+ActiveRecord::Schema.define(version: 20140812142102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140811152651) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
