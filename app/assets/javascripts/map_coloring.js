55e7cfbe8b6cc29a9b210d77ca1f08755dc31c50
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-12 13:50:27 -0400

    WIP: Fixed regex syntax

--- app/assets/javascripts/map_coloring.js
+++ app/assets/javascripts/map_coloring.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var textWithColor = $('#ascii-map').html();
   textWithColor = textWithColor.replace("*", "<span class='character'>*</span>");
-  textWithColor = textWithColor.replace(/\~/g, "<span class='water'>~</span>");
-  textWithColor = textWithColor.replace(/\:/g, "<span class='grass'>:</span>");
+  textWithColor = textWithColor.replace(/~/g, "<span class='water'>~</span>");
+  textWithColor = textWithColor.replace(/:/g, "<span class='grass'>:</span>");
   textWithColor = textWithColor.replace(/T/g, "<span class='forest'>T</span>");
   $('#ascii-map').html(textWithColor);
 }); 
