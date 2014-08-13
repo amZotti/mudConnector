2ced5013b162d250aa2d19e33aeb669f53689263
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-13 19:18:31 -0400

    WIP: NPCs coordinates cannot be null

--- app/models/world.rb
+++ app/models/world.rb
@@ -2,5 +2,4 @@ class World < ActiveRecord::Base
   serialize :terrain
 end
 
-#Activated in seeds.rb
 WORLD = World.first
--- db/migrate/20140813185702_create_non_player_characters.rb
+++ db/migrate/20140813185702_create_non_player_characters.rb
@@ -1,8 +1,8 @@
 class CreateNonPlayerCharacters < ActiveRecord::Migration
   def change
     create_table :non_player_characters do |t|
-      t.integer :x_coordinate
-      t.integer :y_coordinate
+      t.integer :x_coordinate, null: false
+      t.integer :y_coordinate, null: false
 
       t.timestamps null: false
     end
