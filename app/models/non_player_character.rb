5449a0fbb1ddc97b61f5e414d9eca5eb356b7714
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-13 16:26:26 -0400

    WIP: Extracted initialization to seeds.rb instead of trying to do it through console

--- app/models/non_player_character.rb
+++ app/models/non_player_character.rb
@@ -1,5 +1,6 @@
 class NonPlayerCharacter < ActiveRecord::Base
 
+  #Activated in seeds.rb
   def self.generate
     self.create(x_coordinate: rand(38), y_coordinate: rand(10))
   end
--- app/models/world.rb
+++ app/models/world.rb
@@ -1,6 +1,6 @@
 class World < ActiveRecord::Base
   serialize :terrain
 end
-#Create world in console upon db:migrate
-#World.create(terrain: TerrainBuilder.new)
+
+#Activated in seeds.rb
 WORLD = World.first
