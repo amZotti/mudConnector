847ae7b343a4450489a7a32f4683eb13c11ed15e
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-14 10:21:06 -0400

    WIP: Remove unnecessary .self and comments from NPC model

--- app/models/non_player_character.rb
+++ app/models/non_player_character.rb
@@ -1,7 +1,6 @@
 class NonPlayerCharacter < ActiveRecord::Base
 
-  #Activated in seeds.rb
   def self.generate
-    self.create(x_coordinate: rand(38), y_coordinate: rand(10))
+    create(x_coordinate: rand(38), y_coordinate: rand(10))
   end
 end
