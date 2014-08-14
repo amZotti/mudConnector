27b83413968bf78bca5e5eadc6e6d72d78c152bd
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-14 05:04:18 -0400

    WIP: Update schema during seed initialization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811152651) do
+ActiveRecord::Schema.define(version: 20140813185702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20140811152651) do
   end
 
   add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
+
+  create_table "non_player_characters", force: true do |t|
+    t.integer  "x_coordinate", null: false
+    t.integer  "y_coordinate", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
