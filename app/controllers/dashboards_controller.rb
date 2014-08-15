64444787cb7e6fd6a3efcb7a6fe3a9db139b864d
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-15 14:34:23 -0400

    WIP: Colliding characters no longer in controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,5 @@ class DashboardsController < ApplicationController
   def show
     @character = current_user.character
     @map = Map.new(@character).display
-   # @colliding = Character.colliding_with(@character) + NonPlayerCharacter.colliding_with(@character)
   end
 end
