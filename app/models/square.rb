f6f2db77364f6d62164f1861e43d3c604f8c03a9
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-15 10:59:31 -0400

    Players see other characters and their powerlevels when in same square

--- app/models/square.rb
+++ app/models/square.rb
@@ -1,9 +1,9 @@
 class Square
   def self.characters(user)
-    characters = []
-    players = Character.all
-    bots = NonPlayerCharacter.all
-    characters << bots.find_by(x_coordinate: user.x_coordinate, y_coordinate: user.y_coordinate)
-    characters << players.find_by(x_coordinate: user.x_coordinate, y_coordinate: user.y_coordinate)
+    players =  Character.
+      where(x_coordinate: user.x_coordinate, y_coordinate: user.y_coordinate).
+      where.not(id: user.id)
+    npcs = NonPlayerCharacter.where(x_coordinate: user.x_coordinate, y_coordinate: user.y_coordinate)
+    players + npcs
   end
 end
