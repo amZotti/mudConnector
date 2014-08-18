4769541541dcbb55cf17f7c225d05cb7341649e9
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-18 16:05:46 -0400

    WIP: Refactor spacing and hash syntax

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,7 +10,7 @@ class Game
 
   def render_square_display
     other_characters = @character.all_colliding_characters
-    all_characters = {other_characters: other_characters, character: @character}
+    all_characters = { other_characters: other_characters, character: @character }
     Message.display_square_information(all_characters)
     all_characters
   end
