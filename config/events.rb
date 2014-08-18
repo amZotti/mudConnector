cf4fb3495d4bad52f8e3bfd34a420c21c96fe816
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-18 16:26:10 -0400

    WIP: Refactor spacing

--- config/events.rb
+++ config/events.rb
@@ -2,6 +2,7 @@ WebsocketRails::EventMap.describe do
   namespace :attacks do
     subscribe :create, 'attacks#create'
   end
+
   namespace :movements do
     subscribe :create, 'movements#create'
   end
