179746b56c7400cdab466d73024e91c0cc3d1a01
Anthony Zotti <theillbehaviored@gmail.com>
2014-08-20 12:46:16 -0400

    WIP: Remove attacks from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819152449) do
+ActiveRecord::Schema.define(version: 20140820164318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "attacks", force: true do |t|
-    t.integer  "target_id",   null: false
-    t.string   "target_type", null: false
-    t.string   "attack_type", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "attacks", ["target_id"], name: "index_attacks_on_target_id", using: :btree
 
   create_table "characters", force: true do |t|
     t.integer  "user_id",                       null: false
@@ -40,14 +30,13 @@ ActiveRecord::Schema.define(version: 20140819152449) do
   add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
 
   create_table "non_player_characters", force: true do |t|
-    t.integer  "x_coordinate",                                null: false
-    t.integer  "y_coordinate",                                null: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.integer  "power_level",  default: 300,                  null: false
-    t.integer  "energy_level", default: 100,                  null: false
-    t.string   "name",         default: "Frieza henchman",    null: false
-    t.string   "type",         default: "NonPlayerCharacter", null: false
+    t.integer  "x_coordinate",                             null: false
+    t.integer  "y_coordinate",                             null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.integer  "power_level",  default: 300,               null: false
+    t.integer  "energy_level", default: 100,               null: false
+    t.string   "name",         default: "Frieza henchman", null: false
   end
 
   create_table "users", force: true do |t|
